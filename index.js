fc3361ac95bff0e14fc303ef2ee34dbf14721850
nnitesh147 <nniteshprasad147@gmail.com>
2025-03-12 00:36:50 +0530

    changes_in_routes

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.json("Server is running");
+});
+
 // user routes
 app.use("/api/user", userRouter);
 app.use("/api/post", postRouter);
